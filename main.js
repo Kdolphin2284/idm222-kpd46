4c8b0b53dcb4703783ab6d52dc1ac74f8471f26b
Kdolphin2284 <kdolphin2284@gmail.com>
2021-03-07 10:51:29 -0500

    Added ease of access changes
    Navigation is now fixed, my resume download is now fixed, users have the option to click out of the dropdown menu by either pressing the X or by clicking on something else other than the menu.

--- main.js
+++ main.js
@@ -2,23 +2,38 @@ const burgerObj = document.getElementById("burger")
 const menuObj = document.getElementById("menu")
 const xButtonObj = document.getElementById("xButton")
 const menuOptionObj = document.getElementById("menuOption")
+const darkStateObj = document.getElementById("darkenedState")
+
+function noScroll(){
+    window.scrollTo(0,0)
+}
 
 burgerObj.addEventListener("click", function(){
     darkenedState.hidden = !darkenedState.hidden
+    window.addEventListener('scroll', noScroll)
     document.getElementById("menu").style.display = "flex";
     document.getElementById("menu").style.animation = "dropDown .35s ease-in forwards";
     console.log('Burger has been pressed, X is now available')
 })
 
 xButtonObj.addEventListener("click", function(){
+    window.removeEventListener('scroll', noScroll);
     darkenedState.hidden = !darkenedState.hidden
     document.getElementById("menu").style.display = "none";
     console.log('Burger has been closed, burger is now available')
 })
 
 menuOptionObj.addEventListener("click", function(){
-    menu.hidden = !menu.hidden
+    window.removeEventListener('scroll', noScroll);
     darkenedState.hidden = !darkenedState.hidden
+    document.getElementById("menu").style.display = "none";
+    console.log('Burger has been closed, burger is now available')
+})
+
+darkStateObj.addEventListener("click", function(){
+    window.removeEventListener('scroll', noScroll);
+    darkenedState.hidden = !darkenedState.hidden
+    document.getElementById("menu").style.display = "none";
     console.log('Burger has been closed, burger is now available')
 })
 
